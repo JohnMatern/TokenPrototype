f808918dea6158ead49f990ad56993ad5dc39f50
Kurt <Kurt@Merbeth.io>
2021-06-07 17:47:55 +0200

    file upload

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -1,81 +1,100 @@
-import React from 'react';
-import {useState} from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 const { create } = require('ipfs-http-client');
 const ipfs = create(new URL("http://ipfs.infura.io:5001"));
+
 /* eslint-disable no-console */
 const Mint = () => {
-  
+
   const [state, setState] = useState({
     id: "",
     description: "",
   })
-  const [files, setFiles] = useState();
-  
-  function handleChange(evt) {
+  const [file, setFiles] = useState();
+  const [cid, setCid] = useState('')
+
+
+
+
+
+  async function handleChange(evt) {
     const value = evt.target.value;
     setState({
       ...state,
       [evt.target.name]: value
     });
-    const image = evt.target.image;
-
-    setFiles({
-      ...files,
-      file: image,
-    });
-    console.log(files);
-    const options = {
-      wrapWithDirectory: true,
-      progress: (prog) => console.log(`received: ${prog}`)
-    }
   }
+
+  const onFileUpload = async (event) => {
+    setFiles(event.target.files[0])
+  }
+
+
 
   async function submit() {
-    
-    const cid = await ipfs.add(JSON.stringify(state));
- 
-    console.log("IPFS cid:", cid);
-    //console.log(ipfsPath);
-    fetch('https://ipfs.io/ipfs/'+cid.path)
-      .then(response => response.json())
-      .then(data => console.log(data));
+    let randPath = Math.round(Math.random() * 100000);
+    let upload = [];
 
-    ipfs.add(files);
-      
+    upload.push({ path: '/'+randPath+'/meta.json', content: JSON.stringify(state) });
+    upload.push({ path: '/'+randPath+'/' + file.name, content: file });
+
+    ipfs.add(upload).then(
+      response => {
+        dinge(response)
+        console.log(response)
+      }
+    )
+    //console.log(ipfsPath);
+    // fetch('https://ipfs.io/ipfs/' + cid.string)
+    //   .then(response => response.json())
+    //   .then(data => console.log(data));
   }
+
+
+
+  const dinge = (response) => {
+    console.log('aaaaah')
+    setCid(response.string)
+  }
+  useEffect(() => {
+    console.log('cid: '+cid)
+  },[])
+
   return (
     <div className="container contact-form">
-    <div className="contact-image">
-      <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact" />
-    </div>
+      <div className="contact-image">
+        <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact" />
+      </div>
 
-    
-    <div className="row">
-    <h3> Minte deinen NFT</h3>
-      <div className="col-md-12">
-        <div className="form-group">
-          <input type="text" name="id" className="form-control" id="id" placeholder="NFT Name" value={state.id} onChange={handleChange} />
-        </div>
-        <div className="form-group">
-          <input type="text" name="description" className="form-control" id="description" placeholder="Beschreibung" value={state.description} onChange={handleChange} />
-        </div>
-        {/*
+
+      <div className="row">
+        <h3> Minte deinen NFT</h3>
+        <div className="col-md-12">
+          <div className="form-group">
+            <input type="text" name="id" className="form-control" id="id" placeholder="NFT Name" value={state.id} onChange={handleChange} />
+          </div>
+          <div className="form-group">
+            <input type="text" name="description" className="form-control" id="description" placeholder="Beschreibung" value={state.description} onChange={handleChange} />
+          </div>
+          {/*
                     <div className="form-group">
                         <input type="text" name="image" className="form-control" id="image" placeholder="IPFS hash"  />
                     </div>
                     <div className="form-group">
                         <input type="text" name="address" className="form-control" id="address" placeholder="Wallet Addresse"  />
                     </div> */}
-        <label htmlFor="formFile" className="form-label">Lade dein Bild für den NFT hoch!</label>
-        <input className="form-control" type="file" id="file" value={files.file} onChange={handleChange}></input>
-        <div className="form-group">
-          <button type="submit" name="btnSubmit" className="btnContact" value="Mint NFT" onClick={submit}> Mint </button>
+          <label htmlFor="formFile" className="form-label">Lade dein Bild für den NFT hoch!</label>
+          <input className="form-control" type="file" id="file" onChange={onFileUpload}></input>
+          <div className="form-group">
+            <button type="submit" name="btnSubmit" className="btnContact" value="Mint NFT" onClick={submit}> Mint </button>
+          </div>
+        {cid && <a href={'https://ipfs.io/ipfs/'+cid}>Your files on ipfs</a>}
+
         </div>
+
       </div>
 
     </div>
-
-  </div>
   );
 }
 
